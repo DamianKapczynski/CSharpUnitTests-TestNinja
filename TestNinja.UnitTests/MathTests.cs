403378584d1b111c3f69ea94591cfd340d3079b7
Damian Kapczynski <damkapp2@gmail.com>
2024-08-24 14:12:20 +0200

    MathTests

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        private Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        [Test]
+        //[Ignore("To simple")]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            var result = _math.Add(100, 541);
+
+            Assert.AreEqual(result, 641);
+        }
+
+        [Test]
+        [TestCase(5, 2, 5)]
+        [TestCase(-5, 2, 2)]
+        [TestCase(5, 5, 5)]
+        public void Max_WhenCalled_ReturnGreaterArgument(int a, int b, int expectedResult)
+        {
+            var result = _math.Max(a, b);
+
+            Assert.AreEqual(result, expectedResult);
+
+        }
+    }
+}
