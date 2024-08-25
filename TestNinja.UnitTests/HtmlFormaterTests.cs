566318e9cb38117cbc33eb48794104b255bcdf97
Damian Kapczynski <damkapp2@gmail.com>
2024-08-25 11:49:31 +0200

    HtmlFormater Tests

---
+++ TestNinja.UnitTests/HtmlFormaterTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormaterTests
+    {
+        [Test]
+        public void FormatAsBols_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+            //Assert.That(result, Does.StartWith("<strong>"));
+            //Assert.That(result, Does.EndWith("</strong>"));
+            //Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
