037e1388a4fe784ccd9e5f7549937480872ee3a0
Damian Kapczynski <damkapp2@gmail.com>
2024-08-31 16:47:59 +0200

    FizzBuzz Tests

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(9, "Fizz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(11, "11")]
+        public void GetOutput_WhenCalled_ReturnsProperValue(int input, string output)
+        {
+            var FizBuzz = new FizzBuzz();
+
+            var result = FizzBuzz.GetOutput(input);
+
+            Assert.That(result, Is.EqualTo(output));
+        }
+    }
+}
